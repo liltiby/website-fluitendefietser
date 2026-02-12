815fcc49fb59733dd3f2a56a095788c3521d323f
liltiby <wetemansthijmen@gmail.com>
2026-02-12 11:51:02 +0100

    Finished the footer bike

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // test
-var bikePos = 99;
+var bikePos = 98;
 var bike = document.getElementById("biker")
+var biketurn = false
 setInterval(() => {
-bikePos = bikePos - 0.01
+if(bike.style.left == "1vw"){
+    biketurn = true
+    bike.classList.add('bikeflipped')
+    bike.classList.remove('bike')
+}
+if(bike.style.left == "98vw"){
+    biketurn = false
+    bike.classList.remove('bikeflipped')
+    bike.classList.add('bike')
+}
+if(biketurn==true){bikePos = bikePos + 0.05}
+else{bikePos = bikePos - 0.05}
+
 bike.style.left = bikePos + "vw"
-}, 1);
+}, 0.5);
