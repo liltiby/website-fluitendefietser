d4fb48e5bef343762af524707131c2986caa6d8d
liltiby <wetemansthijmen@gmail.com>
2026-02-12 12:45:56 +0100

    made templates for all pages

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ else{bikePos = bikePos - 0.05}
 
 bike.style.left = bikePos + "vw"
 }, 0.5);
+
+
+function keycheck(event){
+let key = event.key
+if(key == 'e' || key == 'E'){
+     var pht = document.getElementById('pht')
+
+     pht.classList.remove('placeholdertext')
+     pht.classList.add('none')
+}
+}
+console.log("Press E to remove placeholder text")
